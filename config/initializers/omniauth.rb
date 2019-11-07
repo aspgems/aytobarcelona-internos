5df3ef038a4abacb323fb70d912f0806752e6a28
Sergio Gómez <sgomez@aspgems.com>
2019-11-07 14:19:34 +0100

    Map attributes and verify user type

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -22,6 +22,10 @@ if Rails.application.secrets.dig(:omniauth, :saml, :enabled)
                     idp_sso_target_url: Chamber.env.saml.idp_sso_target_url,
                     sp_entity_id: Chamber.env.saml.sp_entity_id,
                     strategy_class: ::OmniAuth::Strategies::SAML,
+                    attribute_statements: {
+                      email: ['mail'],
+                      name: ['givenName', 'nom']
+                    },
                     certificate: Chamber.env.saml.certificate,
                     private_key: Chamber.env.saml.private_key,
                     security: {
@@ -32,6 +36,16 @@ if Rails.application.secrets.dig(:omniauth, :saml, :enabled)
 
   Devise::OmniauthCallbacksController.class_eval do
     skip_before_action :verify_authenticity_token
+
+    before_action :verify_user_type, only: :saml
+
+    def verify_user_type
+      saml_response = OneLogin::RubySaml::Response.new(params['SAMLResponse'])
+      unless saml_response.attributes[:tipusUsuari].in? Chamber.env.saml.user_types
+        flash[:error] = I18n.t("devise.failure.invalid_user_type")
+        redirect_to root_path
+      end
+    end
   end
 
   Decidim::User.omniauth_providers << :saml
