26766c4957cca69b75a0a6374d860e1c4b0dec66
David <dperez@aspgems.com>
2019-11-11 08:12:53 +0100

    SAML integration (#11)
    * Added saml integration
    
    * added signed authn requests
    
    * Map attributes and verify user type
    
    * added production settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -15,4 +15,40 @@ if Rails.application.secrets.dig(:omniauth, :imipre, :enabled)
   Decidim::User.omniauth_providers << :imipre
 end
 
+if Rails.application.secrets.dig(:omniauth, :saml, :enabled)
+  Devise.setup do |config|
+    config.omniauth :saml,
+                    idp_cert: Chamber.env.saml.idp_cert,
+                    idp_sso_target_url: Chamber.env.saml.idp_sso_target_url,
+                    sp_entity_id: Chamber.env.saml.sp_entity_id,
+                    strategy_class: ::OmniAuth::Strategies::SAML,
+                    attribute_statements: {
+                      email: ['mail'],
+                      name: ['givenName', 'nom']
+                    },
+                    certificate: Chamber.env.saml.certificate,
+                    private_key: Chamber.env.saml.private_key,
+                    security: {
+                      authn_requests_signed: true,
+                      signature_method: XMLSecurity::Document::RSA_SHA256
+                    }
+  end
+
+  Devise::OmniauthCallbacksController.class_eval do
+    skip_before_action :verify_authenticity_token
+
+    before_action :verify_user_type, only: :saml
+
+    def verify_user_type
+      saml_response = OneLogin::RubySaml::Response.new(params['SAMLResponse'])
+      unless saml_response.attributes[:tipusUsuari].in? Chamber.env.saml.user_types
+        flash[:error] = I18n.t("devise.failure.invalid_user_type")
+        redirect_to root_path
+      end
+    end
+  end
+
+  Decidim::User.omniauth_providers << :saml
+end
+
 OmniAuth.config.logger = Rails.logger
